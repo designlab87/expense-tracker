49864e53c677ef373ae2da3c9b895e805a5d262c
designlab87 <sethwatson1976@gmail.com>
2022-10-05 11:26:15 -0500

    UI updates
    Cleaned up UI. Next step, connect to google sheets

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
-  { id: 'e1', title: 'Toilet Paper',      amount: 94.12,  date: new Date(2020, 7, 14), },
-  { id: 'e2', title: 'New TV',            amount: 799.49, date: new Date(2021, 2, 12) },
-  { id: 'e3', title: 'Car Insurance',     amount: 294.67, date: new Date(2021, 2, 28), },
-  { id: 'e4', title: 'New Desk (Wooden)', amount: 450,    date: new Date(2021, 5, 12), },
+  { id: 'e1', title: 'Toilet Paper',            amount: 1894.12,    date: new Date(2020, 7, 14), },
+  { id: 'e2', title: 'New TV',                  amount: 799.49,     date: new Date(2021, 2, 12) },
+  { id: 'e3', title: 'New Puppy',               amount: 294.67,     date: new Date(2021, 2, 28), },
+  { id: 'e4', title: 'New Desk',                amount: 450.33,     date: new Date(2021, 5, 12), },
+  { id: 'e5', title: 'Matcha',                  amount: 37.99,   date: new Date(2022, 2, 5), },
+  { id: 'e6', title: 'Ashwagandha',             amount: 50.99,      date: new Date(2022, 7, 21), },
+  { id: 'e7', title: 'Yoga Mat',                amount: 29.99,      date: new Date(2022, 8, 6), }
 ];
 
 const App = () => {
